d264c14482adf98bc677f7ab067dec63e07f38bf
Adam Łyskawa <a.lyskawa@gmail.com>
2025-05-02 12:34:55 +0200

    FIX: Remove sizes from arguments.
    This shows how cool our template trick works.

--- Keyboard_320x200/gui/src/demo_screen/DemoView.cpp
+++ Keyboard_320x200/gui/src/demo_screen/DemoView.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 DemoView::DemoView()
-:   editable1(Editable(line1, line1Buffer, LINE1_SIZE)),
-    editable2(Editable(line2, line2Buffer, LINE2_SIZE)),
+:   editable1(Editable(line1, line1Buffer)),
+    editable2(Editable(line2, line2Buffer)),
     text1(), text2() { }
 
 void DemoView::line1Edit()
@@ -23,8 +23,8 @@ void DemoView::editCancel()
 
 void DemoView::editApply()
 {
-    if (keyboard.targets(editable1)) editable1.toUTF8(text1, LINE1_SIZE);
-    else if (keyboard.targets(editable2)) editable1.toUTF8(text2, LINE2_SIZE);
+    if (keyboard.targets(editable1)) editable1.toUTF8(text1);
+    else if (keyboard.targets(editable2)) editable1.toUTF8(text2);
 }
 
 void DemoView::keyPressed()
